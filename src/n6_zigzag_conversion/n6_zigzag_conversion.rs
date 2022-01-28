1a09ad1cf2c9853f08a50d7094b617cd51b707ef
古飞 <goofy.lp@gmail.com>
2022-01-28 11:58:42 +0800

    perfect n6 0ms 2.2M

--- src/n6_zigzag_conversion/n6_zigzag_conversion.rs
+++ src/n6_zigzag_conversion/n6_zigzag_conversion.rs
@@ -1,10 +1,38 @@
 pub fn main() {
     // println!("{}", longest_palindrome(String::from("eeee")));
-    convert_and_print(String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 27);
+    convert_and_print(String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 6);
+    println!("{}", convert(String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 6));
 }
 
 pub fn convert(s: String, num_rows: i32) -> String {
-    s
+    let chars = s.as_bytes();
+    let len: i32 = chars.len() as i32;
+    let mut result = Vec::with_capacity(len as usize);
+    let chars_in_group = if num_rows==1 {1} else {num_rows * 2 - 2};
+    let groups = (len - 1) / chars_in_group + 1;
+
+    let mut i = 0;
+    for row in 0..num_rows {
+        // let mut group = 0;
+        for group in 0..groups {
+            let first_char_index = chars_in_group * group + row;
+            if first_char_index < len {
+                result.push(chars[first_char_index as usize]);
+                // result[i] = chars[first_char_index as usize];
+                // i+=1;
+            }
+            if row != 0 && row != num_rows - 1 {
+                let second_char_index = chars_in_group * group + num_rows * 2 - row - 2;
+                if second_char_index < len {
+                    result.push(chars[second_char_index as usize]);
+                    // result[i] = chars[second_char_index as usize];
+                    // i+=1;
+                }
+            }
+        }
+    }
+
+    String::from_utf8(result).expect("Invalid utf-8 in [u8].")
 }
 pub fn convert_and_print(s: String, num_rows: i32) {
     let chars = s.as_bytes();

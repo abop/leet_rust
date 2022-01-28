b8c9fe8be3bc87abe66fe22739cd7d69f08021a9
古飞 <goofy.lp@gmail.com>
2022-01-28 12:00:34 +0800

    n6 clean code

--- src/n6_zigzag_conversion/n6_zigzag_conversion.rs
+++ src/n6_zigzag_conversion/n6_zigzag_conversion.rs
@@ -11,7 +11,7 @@ pub fn convert(s: String, num_rows: i32) -> String {
     let chars_in_group = if num_rows==1 {1} else {num_rows * 2 - 2};
     let groups = (len - 1) / chars_in_group + 1;
 
-    let mut i = 0;
+    // let mut i = 0;
     for row in 0..num_rows {
         // let mut group = 0;
         for group in 0..groups {
@@ -64,40 +64,4 @@ pub fn convert_and_print(s: String, num_rows: i32) {
         }
         println!();
     }
-}
-
-pub fn longest_palindrome(s: String) -> String {
-    let chars = s.as_bytes();
-    let len = chars.len();
-    if len <= 1 {
-        return s;
-    }
-    let mut min = 0;
-    let mut max = 0;
-    let mut core = 0;
-    let mut left;
-    let mut right;
-    while core < len - 1 {
-        left = core;
-        right = core;
-
-        while right + 1 < len && chars[core] == chars[right + 1] {
-            right += 1;
-        }
-        core = (left + right) / 2 + 1;
-
-        while left >= 1 && right + 1 < len {
-            if chars[left - 1] == chars[right + 1] {
-                left -= 1;
-                right += 1;
-            } else {
-                break;
-            }
-        }
-        if right - left > max - min {
-            max = right;
-            min = left;
-        }
-    }
-    return String::from(&s[min..max + 1]);
 }
